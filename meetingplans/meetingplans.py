97249a3507a9bd79dd3de1dede84a867e094572b
Hridaan965 <hridaansarvagyaa@gmail.com>
2022-07-21 21:50:17 +0530

    initial commit

---
+++ meetingplans/meetingplans.py
@@ -0,0 +1,18 @@
+from redbot.core import commands
+
+class MeetingPlans(commands.Cog):
+    """Purges the meeting plans channel!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """Purges the meeting plans channel!"""
+        
+        from discord import discord
+        @client.event
+        async def on_message(message):
+          if not message.content == ":AnnoyingMiddleFinger:":
+            await message.delete()
+            await message.author.send(f"You can not send {message.content} in that channel.")
