302436754e51aeb5be98f14820a19c5c39beb8f4
Sparkzzzzzz <56631696+Sparkzzzzzz@users.noreply.github.com>
2022-08-05 22:49:17 +0530

    Create config.py

---
+++ serverstats/config.py
@@ -0,0 +1,27 @@
+#Server Member Count#
+ServerMembersCheckEnabled = True                            #True for enabled, False for disabled
+IDofChannelForServerMembers = 000000000000000000            #ID of Voice channel to update with server member count
+NameofStatsChannel1 = str("All Members: ")                  #Adjusts the name of the Voice channel, LEAVE THE SPACE!
+
+
+#Role 1 Count #                                      
+Role1CountCheckEnabled = True                              #True for enabled, False for disabled
+IDofRole1 = 000000000000000000                             #ID of the server role to keep track of
+IDofChannelForRole1Check = 000000000000000000              #ID of Voice channel to update with the Role1 count with the role specified above
+NameofRole1Channel = str("Role 1: ")                       #Adjusts the name of the Voice channel, LEAVE THE SPACE!
+
+#Role 2 Count #
+Role2CountCheckEnabled = True                              #True for enabled, False for disabled
+IDofRole2 = 000000000000000000                             #ID of the server role to keep track of
+IDofChannelForRole2Check = 000000000000000000              #ID of Voice channel to update with the Role2 count with the role specified above
+NameofRole2Channel = str("Role 2: ")                       #Adjusts the name of the Voice channel, LEAVE THE SPACE!
+
+#Bot Count #
+BotCountCheckEnabled = True                                #True for enabled, False for disabled
+IDofChannelForBotCheck = 000000000000000000                #ID of Voice channel to update with the bot count
+NameofBotChannel = str("Bots: ")                           #Adjusts the name of the Voice channel, LEAVE THE SPACE!
+
+#Bans Count #                                     
+BansCountCheckEnabled = True                               #True for enabled, False for disabled  
+IDofChannelForBansCheck = 000000000000000000               #ID of Voice channel to update with the server ban count
+NameofBansChannel = str("Bans: ")                          #Adjusts the name of the Voice channel, LEAVE THE SPACE!
