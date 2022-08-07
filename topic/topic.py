11a691bc331daa3c0463e62baf21f404d520eebb
Hridaan965 <hridaansarvagyaa@gmail.com>
2022-08-07 19:53:36 +0530

    edited topic.py

--- topic/topic.py
+++ topic/topic.py
@@ -12,16 +12,12 @@ class Topic(commands.Cog):
     @commands.command()
     async def topic(self, ctx):
         """Sends a random conversation initiating topic when invoked!!"""
-        
+        url = "https://conversation-starter1.p.rapidapi.com/"
+        headers = {
+	      "X-RapidAPI-Key": "4cde062ea1msh26fabf868841d38p1e4291jsn80bedfad328e",
+	      "X-RapidAPI-Host": "conversation-starter1.p.rapidapi.com"
+                  }
 
-url = "https://conversation-starter1.p.rapidapi.com/"
-
-headers = {
-	"X-RapidAPI-Key": "4cde062ea1msh26fabf868841d38p1e4291jsn80bedfad328e",
-	"X-RapidAPI-Host": "conversation-starter1.p.rapidapi.com"
-}
-
-response = request.request("GET", url, headers=headers)
-
-print(response.text)
+        response = request.request("GET", url, headers=headers)
+        print(response.text)
         
