b4a414e06490d343cc85c0dd2da979d9709fcbfe
Hridaan965 <hridaansarvagyaa@gmail.com>
2022-09-04 00:17:32 +0530

    added command descriptions

--- scceval/scceval.py
+++ scceval/scceval.py
@@ -12,11 +12,13 @@ class SccEval(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def uthere(self, ctx):
+      """verifies bot presence"""
       await ctx.channel.send("I am here at your service <@777788426714873877>!")
       
     @commands.command()
     @commands.is_owner()
     async def verifyme(self, ctx):
+      """verifies that the user running the command is the bot owner"""
       await ctx.channel.send("<@777788426714873877> you are Sparkz sir, my dear bot owner.")
 
 
--- usefulinfo/usefulinfo.py
+++ usefulinfo/usefulinfo.py
@@ -11,8 +11,10 @@ class UsefulInfo(commands.Cog):
 
     @commands.command()
     async def creator(self, ctx):
+     """tells who made the bot"""
      await ctx.channel.send("I was made by my bot owner **Sparkz#2645**!")
     
     @commands.command()
     async def version(self, ctx):
+     """tells the current bot version"""
      await ctx.channel.send("version: `0.0.1`")   
