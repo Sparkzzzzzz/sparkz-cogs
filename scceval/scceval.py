9ecc245cba7694b19022ddb4bac4b5c6bfb0224d
Sparkzzzzzz <56631696+Sparkzzzzzz@users.noreply.github.com>
2022-09-04 11:25:36 +0530

    Update scceval.py

--- scceval/scceval.py
+++ scceval/scceval.py
@@ -11,14 +11,14 @@ class SccEval(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def uthere(self, ctx):
-      """Verifies bot presence."""
-      await ctx.channel.send("I am here at your service <@777788426714873877>!")
+     """Verifies bot presence."""
+     await ctx.channel.send("I am here at your service <@777788426714873877>!")
       
     @commands.command()
     @commands.is_owner()
     async def verifyme(self, ctx):
-      """Verifies that the user running the command is the bot owner.""
-      await ctx.channel.send("You are <@777788426714873877> sir, my dear bot owner.")
+     """Verifies that the user running the command is the bot owner.""
+     await ctx.channel.send("You are <@777788426714873877> sir, my dear bot owner.")
 
 
    
