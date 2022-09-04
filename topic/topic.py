9528ef5b7815514e14bedb99e0842d634bd4f9ed
Hridaan965 <hridaansarvagyaa@gmail.com>
2022-09-04 13:28:01 +0530

    updated topic cog

--- topic/topic.py
+++ topic/topic.py
@@ -1,6 +1,6 @@
-import requests
 import discord
 from discord import client
+import random
 from redbot.core import commands
 
 class Topic(commands.Cog):
@@ -11,13 +11,8 @@ class Topic(commands.Cog):
 
     @commands.command()
     async def topic(self, ctx):
-        """Sends a random conversation initiating topic when invoked!!"""
-        url = "https://conversation-starter1.p.rapidapi.com/"
-        headers = {
-	      "X-RapidAPI-Key": "4cde062ea1msh26fabf868841d38p1e4291jsn80bedfad328e",
-	      "X-RapidAPI-Host": "conversation-starter1.p.rapidapi.com"
-                  }
-
-        response = requests.request("GET", url, headers=headers)
-        print(response.text)
+     """Sends a random conversation initiating topic when invoked."""
+     await ctx.channel.send(random.choice(msg_list))
+     msg_list = ["What is something you can tell me about yourself that will help me remember you over others?", "Some believe that our identity is directly correlated to our career. What is your thought?", "What is the most difficult thing for you about living in this city?", "If money wasn’t a necessity, what career would you choose for yourself?", "How do you separate what joy is from happiness?", "What’s your favorite restaurant and why?", "Name three words that best describe you.", "What’s the best Wi-Fi network name you have ever seen?", "What candy bar would you be?", "If you could hack any computer, whose computer would you pick?"]
+    
         
