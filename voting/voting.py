4299a7c370d72f55d1e04232421697443e47cf18
Hridaan965 <hridaansarvagyaa@gmail.com>
2022-12-10 22:51:53 +0530

    added voting class to the cog

--- voting/voting.py
+++ voting/voting.py
@@ -2,19 +2,25 @@ import discord
 from redbot.core import commands
 import interactions
 
-bot = interactions.Client
+class Voting(commands.Cog):
+    """Slash tag that sends a voting link when invoked."""
 
-@bot.command(
-    name="vote",
-    description="Vote for CeneVspeed Clan!",
-    scope=916294593228714014,
-)
+    def __init__(self, bot):
+        self.bot = bot
+        
+    bot = interactions.Client
 
-async def vote(self, ctx: interactions.CommandContext, ephemeral=True):
-    embed = discord.Embed(title="Vote for CeneVspeed Clan!", description="https://top.gg/servers/916294593228714014") #,color=Hex code
-    embed.add_field(name="Perks:", value="")
-    embed.add_field(name="", value="`>` Access to <#979594849273643048>")
-    embed.add_field(name="", value="`>` Hoisted role!")
-    embed.add_field(name="", value="`>` Access to <#952146169105104906>")
+    @bot.command(
+        name="vote",
+        description="Vote for CeneVspeed Clan!",
+        scope=916294593228714014,
+    )
+
+    async def vote(ctx: interactions.CommandContext):
+        embed = discord.Embed(title="Vote for CeneVspeed Clan!", description="https://top.gg/servers/916294593228714014") #,color=Hex code
+        embed.add_field(name="Perks:", value="")
+        embed.add_field(name="", value="`>` Access to <#979594849273643048>")
+        embed.add_field(name="", value="`>` Hoisted role!")
+        embed.add_field(name="", value="`>` Access to <#952146169105104906>")
     
-    interactions.response.send(embed=embed, ephemeral=True)
+        interactions.response.send(embed=embed, ephemeral=True)
