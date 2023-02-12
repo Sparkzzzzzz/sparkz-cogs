70441b78f3f676eb2ff384bce0743693a24f92b5
Sparkzzzzzz <hridaansarvagyaa@gmail.com>
2023-02-12 22:02:03 +0530

    edited funmsg cog

--- funmsg/funmsg.py
+++ funmsg/funmsg.py
@@ -20,7 +20,7 @@ class FunMsg(commands.Cog):
     intents.members = True
     intents.reactions = True
 
-    client = commands.Bot(intents=intents, command_prefix=c<)
+    client = commands.Bot(intents=intents, command_prefix="c<")
 
 
     DefaultClearAmmount = 5
