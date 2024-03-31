858e47424db6d019717a052bfb965afea60cd140
Sparkzzzzzz <hridaansarvagyaa@gmail.com>
2024-03-31 22:07:48 +0530

    fixed neilshit 1

--- neilshit/neilshit.py
+++ neilshit/neilshit.py
@@ -11,57 +11,56 @@ class NeilShit(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def eball(ctx, *, sentence):
+    async def eball(self, ctx, *, sentence):
         """Ask a question!"""
 
         responses = [
-        "It is certain.",
-        "It is decidedly so.",
-        "Without a doubt.",
-        "Yes – definitely.",
-        "You may rely on it.",
-        "As I see it, yes.",
-        "Most likely.",
-        "Outlook good.",
-        "Yes.",
-        "Signs point to yes.",
-        "Reply hazy, try again.",
-        "Ask again later.",
-        "Better not tell you now.",
-        "Cannot predict now.",
-        "Concentrate and ask again.",
-        "Don't count on it.",
-        "My reply is no.",
-        "My sources say no.",
-        "Outlook not so good.",
-        "Very doubtful."
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
         ]
 
-        response = random.choice(responses)
-
-        if "Neil" in sentence():
+        if "Neil" in sentence:
             a = "Ew if you fucking used the word Neil in this sentence then I wont fucking dignify it with a response."
-            await ctx.channel.send(a)
+            await ctx.send(a)
 
-        elif "neil" in sentence():
+        elif "neil" in sentence:
             a = "Ew if you fucking used the word Neil in this sentence then I wont fucking dignify it with a response."
-            await ctx.channel.send(a)
+            await ctx.send(a)
 
-        elif "Sparkz" in sentence():
+        elif "Sparkz" in sentence:
             b = "Sparkz is goated."
-            await ctx.channel.send(b)
+            await ctx.send(b)
 
-        elif "sparkz" in sentence():
+        elif "sparkz" in sentence:
             b = "Sparkz is goated."
-            await ctx.channel.send(b)
+            await ctx.send(b)
 
-        elif "Hridaan" in sentence():
+        elif "Hridaan" in sentence:
             c = "Hridaan is goated."
-            await ctx.channel.send(c)
+            await ctx.send(c)
 
-        elif "hridaan" in sentence():
+        elif "hridaan" in sentence:
             c = "Hridaan is goated."
-            await ctx.channel.send(c)
+            await ctx.send(c)
 
         else:
-            await ctx.channel.send(response)
+            response = random.choice(responses)
+            await ctx.send(response)
