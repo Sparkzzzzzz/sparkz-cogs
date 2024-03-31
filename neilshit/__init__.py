598fa0127fe45b2f3dd6cd61e329e6301d549d87
Sparkzzzzzz <56631696+Sparkzzzzzz@users.noreply.github.com>
2024-03-31 21:50:45 +0530

    added neilshit

---
+++ neilshit/__init__.py
@@ -0,0 +1,7 @@
+from .neilshit import NeilShit
+import discord
+import random
+
+
+def setup(bot):
+    bot.add_cog(NeilShit(bot))
---
+++ neilshit/neilshit.py
@@ -0,0 +1,67 @@
+import discord
+from discord import client
+from redbot.core import commands
+import random
+
+
+class NeilShit(commands.Cog):
+    """Neil is shit!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def eball(ctx, *, sentence):
+        """Ask a question!"""
+
+        responses = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes – definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+        ]
+
+        response = random.choice(responses)
+
+        if "Neil" in sentence():
+            a = "Ew if you fucking used the word Neil in this sentence then I wont fucking dignify it with a response."
+            await ctx.channel.send(a)
+
+        elif "neil" in sentence():
+            a = "Ew if you fucking used the word Neil in this sentence then I wont fucking dignify it with a response."
+            await ctx.channel.send(a)
+
+        elif "Sparkz" in sentence():
+            b = "Sparkz is goated."
+            await ctx.channel.send(b)
+
+        elif "sparkz" in sentence():
+            b = "Sparkz is goated."
+            await ctx.channel.send(b)
+
+        elif "Hridaan" in sentence():
+            c = "Hridaan is goated."
+            await ctx.channel.send(c)
+
+        elif "hridaan" in sentence():
+            c = "Hridaan is goated."
+            await ctx.channel.send(c)
+
+        else:
+            await ctx.channel.send(response)
