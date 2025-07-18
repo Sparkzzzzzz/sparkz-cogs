bc9ac19c1eb81ed81762cda05d2a50f8c5d7e0ab
Sparkzzzzzz <hridaansarvagyaa@gmail.com>
2025-07-19 00:17:07 +0530

    match events.py (onetrueslasg fix 3)

--- onetrueslash/events.py
+++ onetrueslash/events.py
@@ -4,7 +4,7 @@ import discord
 from redbot.core import commands as red_commands
 from redbot.core.bot import Red
 
-from .commands import onetrueslash
+from .commands import command
 from .utils import valid_app_name
 
 
@@ -25,19 +25,19 @@ async def on_user_update(before: discord.User, after: discord.User):
         return
     if before.name == after.name:
         return
-    old_name = onetrueslash.name
+    old_name = command.name
     try:
-        onetrueslash.name = valid_app_name(after.name)
+        command.name = valid_app_name(after.name)
     except ValueError:
         await bot.send_to_owners(
-            f"`onetrueslash` was unable to make the name {after.name!r} "
+            f"`command` was unable to make the name {after.name!r} "
             "into a valid slash command name. The command name was left unchanged."
         )
         return
     bot.tree.remove_command(old_name)
-    bot.tree.add_command(onetrueslash, guild=None)
+    bot.tree.add_command(command, guild=None)
     await bot.send_to_owners(
-        "The bot's username has changed. `onetrueslash`'s slash command has been updated to reflect this.\n"
+        "The bot's username has changed. `command`'s slash command has been updated to reflect this.\n"
         "**You will need to re-sync the command tree yourself to see this change.**\n"
         "It is recommended not to change the bot's name too often with this cog, as this can potentially "
         "create confusion for users as well as ratelimiting issues for the bot."
